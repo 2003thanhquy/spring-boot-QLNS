fa3ccc2280f64bc428ecd6ac81aa9ec9580d7047
2003thanhquy <2003thanhquy>
2024-01-20 22:19:53 +0700

    create project

---
+++ src/test/java/org/spkt/qlnhansu/QlNhanSuApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spkt.qlnhansu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlNhanSuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
